2028e950dd5b4f7ffd6f3ea6f69a16af99f22b77
Matheus Larangeira <matheus-larangeira@hotmail.com>
2025-10-24 16:31:39 -0300

    rota criar filme

---
+++ back-end/main.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI
+import funcao
+
+#Como exeutar o fastapi
+# python -m uvicorn main:app --reload
+app = FastAPI(title="Gerenciador de filmes")
+
+#Criando uma rota
+@app.get("/")
+def home():
+    return {"mensagem": "Bem-vindo ao gerenciador de filmes"}
+
+@app.post("/filmes")
+def criar_filme(titulo: str, genero: str, ano: int, nota: float):
+    funcao.cadastrar_filme(titulo, genero, ano, nota)
+    return {"200": "Filme cadastrado com sucessso!"}
+
+
